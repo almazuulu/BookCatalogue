ed8d977e06363e7980c5c41bcb4377aa75acbc78
almazuulu <almazuulu@gmail.com>
2023-10-03 20:02:15 +0600

    Модели и миграции Django

--- bookcatalogue/book/admin.py
+++ bookcatalogue/book/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Author, Book, Genre, Review
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['author_name']
+    search_fields = ['author_name']
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['genre_name']
+    search_fields = ['genre_name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'author', 'publish_date']
+    list_filter = ['genre', 'author']
+    search_fields = ['title', 'genre']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['book', 'user', 'rating']
+    search_fields = ['name']
--- bookcatalogue/book/models.py
+++ bookcatalogue/book/models.py
@@ -1,3 +1,73 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext as _  # Для многоязычности
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    favorites = models.ManyToManyField('Book', related_name='users_favorited')
+    
+    # Переопределение связанных полей
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'Группы к которым этот пользователь принадлежит. Пользователь получит все разрешения'
+        ),
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Конкретные разрешения'),
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author_name = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.author_name
+
+
+class Genre(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    genre_name = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.genre_name
+
+
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    publish_date = models.DateField()
+
+    @property
+    def average_rating(self):
+        return self.reviews.aggregate(models.Avg('rating'))['rating__avg'] or 0
+    
+    def __str__(self) -> str:
+        return self.title
+
+
+class Review(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
+    
+    def __str__(self) -> str:
+        return f'Rating for {self.book}'
