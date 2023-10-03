1885e106278c5c591c414661fe23685a5cf9de91
almazuulu <almazuulu@gmail.com>
2023-10-03 20:47:42 +0600

    Настройка шаблона маршрута и представления

--- bookcatalogue/book/views.py
+++ bookcatalogue/book/views.py
@@ -1,3 +1,34 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView 
 
-# Create your views here.
+
+from .models import Book, Genre, Author 
+
+class BookListView(ListView): 
+    model = Book
+    template_name = 'book/index.html'
+    context_object_name = 'books'
+    paginate_by = 10 # Для пагинации на 10 книг на страницу
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = Book.objects.all()
+        
+        # Фильтрация по жанру
+        genre = self.request.GET.get('genre')
+        if genre:
+            queryset = queryset.filter(genre__genre_name=genre)
+        
+        # Фильтрация по автору
+        author = self.request.GET.get('author')
+        if author:
+            queryset = queryset.filter(author__author_name=author)
+        
+        # Фильтрация по датам
+        date_from = self.request.GET.get('date_from')
+        date_to = self.request.GET.get('date_to')
+        if date_from and date_to:
+            queryset = queryset.filter(publish_date__range=[date_from, date_to])
+
+        return queryset
